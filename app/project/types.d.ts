61f708fe5aa86c9b6280f3c3c70f43dccffed7d1
ghulam-mujtaba5 <ghulammujtaba1005@gmail.com>
2025-06-05 23:20:36 +0500

    refactor: remove ProjectShowcase, ProjectStack, ProjectStats, ProjectTechnologies, ProjectTestimonials, and ProjectTimeline components along with their styles for a cleaner codebase

--- app/project/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@/components/ProjectOverview/ProjectOverview' {
-    const ProjectOverview: React.FC;
-    export default ProjectOverview;
-}
-
-declare module '@/components/ProjectStack/ProjectStack' {
-    const ProjectStack: React.FC;
-    export default ProjectStack;
-}
