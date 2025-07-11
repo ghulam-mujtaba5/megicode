9da8fc0e91d52710b72b884d06c4dffc3e2a1128
MegiCode <megicode@gmail.com>
2025-07-12 01:10:09 +0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,6 +7,7 @@ import dynamic from 'next/dynamic';
 import Loading from '../loading';
 
 
+
 // Static imports for critical components
 import NavBarDesktop from "../../components/NavBar_Desktop_Company/nav-bar-Company";
 import NavBarMobile from "../../components/NavBar_Mobile/NavBar-mobile";
