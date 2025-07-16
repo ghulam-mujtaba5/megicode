57c1d563a9a74f64d2409549d8693b413679566e
MegiCode <megicode@gmail.com>
2025-07-16 21:09:23 +0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,6 @@ import LoadingAnimation from '@/components/LoadingAnimation/LoadingAnimation';
 
 
 
-
 // Static imports for critical components
 import NavBarDesktop from "../../components/NavBar_Desktop_Company/nav-bar-Company";
 import NavBarMobile from "../../components/NavBar_Mobile/NavBar-mobile";
