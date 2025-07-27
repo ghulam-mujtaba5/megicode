22d04e4d1f209658351358a180bf9100acd3bb7a
ghulam-mujtaba5 <ghulammujtaba1005@gmail.com>
2025-07-28 00:06:40 +0500

    .

--- app/services/[slug]/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <main style={{ maxWidth: 600, margin: '0 auto', padding: '2rem 1rem', textAlign: 'center' }}>
-      <h1 style={{ fontSize: '2rem', color: '#4ea8ff', fontWeight: 800 }}>Service Not Found</h1>
-      <p style={{ fontSize: '1.1rem', color: '#555', marginTop: 16 }}>Sorry, the service you are looking for does not exist.</p>
-    </main>
-  );
-}
