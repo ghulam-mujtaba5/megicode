79ff3be669b21deda1a99163f2031b8913551bc5
ghulam-mujtaba5 <ghulammujtaba1005@gmail.com>
2025-07-27 18:16:25 +0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import HomePageClient from "./HomePageClient";
 
 export const metadata = {
-  title: "Custom Software, Web & Mobile App Development | Megicode",
+  title: "Megicode",
   description: "Megicode delivers high-performance custom software solutions, including web and mobile applications, AI-driven systems, and data analytics. Partner with us to build your next big idea.",
 };
 
